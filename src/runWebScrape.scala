bf69adbbe4aa780e0146469b0ae67623e4a874c3
nicsut <nicsut16@gmail.com>
2022-05-06 11:50:33 -0400

    mmm

--- src/runWebScrape.scala
+++ src/runWebScrape.scala
@@ -1,5 +1,9 @@
+import java.io._
 object runWebScrape {
   def main(args: Array[String]): Unit = {
-
+    val pw= new PrintWriter(new File("OutPut/comics.html"))
+    pw.write("<html> \n <head><title>My Comics Page</title></head> \n")
+    pw.write("<body>\n this is a webpage </body> \n </html>")
+    pw.close()
   }
 }
